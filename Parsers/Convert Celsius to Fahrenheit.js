d9554f7271025ff1c8d574b8171f221a30ef8057
Kyle Burns <kylburns89@gmail.com>
2024-10-05 19:47:27 -0500

    Update Convert Celsius to Fahrenheit.js
    Modified parser to convert all Fahrenheit temperatures in the input string to Celsius instead of only the first temp.

--- Parsers/Convert Celsius to Fahrenheit.js
+++ Parsers/Convert Celsius to Fahrenheit.js
@@ -1,13 +1,24 @@
 /*
-activation_example:30c or 30 degrees celsius
-regex:(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?c(?:elsius|elcius)?\b
+activation_example:30f or 30 degrees Fahrenheit
+regex:(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?f(?:ahrenheit)?\b
 flags:gmi
 */
 
-var regextest = /(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?c(?:elsius|elcius)?\b/gmi;
-var match = regextest.exec(current.text);
+var regextest = /(?:^|\s)(-?\d{1,3}\.?\d{0,2})°?\s?(?:degrees)?\s?f(?:ahrenheit)?\b/gmi;
 var numbertest = /-?\d{1,}\.?\d{0,}/;
-var numbermatch = numbertest.exec(match[0]);
-var ctof = (parseFloat(numbermatch[0]) * 9/5) + 32;
-var originalnumber = parseFloat(numbermatch[0]).toFixed(2).toString().slice(-3) == '.00' ? parseFloat(numbermatch[0]).toFixed(2).toString().slice(0, -3) : parseFloat(numbermatch[0]).toFixed(2);
-var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, originalnumber + '°C is ' + ctof.toFixed(2) + ' degrees in freedom units (Fahrenheit).');
+var conversions = [];
+
+var match;
+while ((match = regextest.exec(current.text)) !== null) {
+    var numbermatch = numbertest.exec(match[0]);
+    var fahrenheit = parseFloat(numbermatch[0]);
+    var celsius = (fahrenheit - 32) * 5/9;
+    
+    var originalNumber = fahrenheit.toFixed(2).toString().slice(-3) == '.00' ? 
+        fahrenheit.toFixed(0) : fahrenheit.toFixed(2);
+    
+    conversions.push(originalNumber + '°F is ' + celsius.toFixed(2) + ' degrees in sane units (Celsius).');
+    current.text = current.text.replace(match[0], celsius.toFixed(2) + '°C');
+}
+var conversionMessage = conversions.join('\n');
+var send_chat = new x_snc_slackerbot.Slacker().send_chat(current, conversionMessage);
